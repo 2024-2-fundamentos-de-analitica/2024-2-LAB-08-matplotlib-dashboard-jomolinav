ff374cff4cfb0420da64c9ddf47e3a89ad2e5d88
jomolinav <jomolinav@unal.edu.co>
2025-01-09 19:49:14 -0500

    Lab8upd1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,35 +3,79 @@
 Escriba el codigo que ejecute la accion solicitada.
 """
 
-
 def pregunta_01():
+    import matplotlib.pyplot as plt 
+    import pandas as pd 
+    import os 
+
+    if not os.path.exists("docs"):
+        os.makedirs("docs")
+
+    df = pd.read_csv("files/input/shipping-data.csv")
+
+    df.head()
+    print(df.head())
+
+    def shware(df):
+        df = df.copy()
+        plt.figure()
+        counts = df.Warehouse_block.value_counts()
+        counts.plot.bar(
+                title="envios por almacenamiento"
+            )
+        plt.savefig("docs/shipping_per_warehouse.png")
+    shware(df)
+
+    def vimoshi(df):
+        df = df.copy()
+        plt.figure()
+        counts = df.Mode_of_Shipment.value_counts()
+        counts.plot.pie(
+            title = "Forma de envío"
+        )
+        plt.savefig("docs/mode_of_shipment.png")
+
+    vimoshi(df)
+
+    def vicusrat(df):
+        df = df.copy()
+        plt.figure()
+        df = (
+            df[["Mode_of_Shipment", "Customer_rating"]].groupby("Mode_of_Shipment").describe
+            
+        )
+        #df.columns = df.columns.droplevel()
+        #df = df[["mean", "min", "max"]]
+        plt.savefig("docs/average_customer_rating.png")
+        return df
+    vicusrat(df)
+
+    def wedi(df):
+        df = df.copy()
+        plt.figure()
+        df.Weight_in_gms.plot.hist()
+        plt.savefig("docs/weight_distribution.png")
+    wedi(df)
+
+    html_content = """<!DOCTYPE html>
+    <html>
+    <body>
+        <h1>Shipping Dashboard Example</h1>
+        <div style="width:45%;float:left">
+        <img src="shipping_per_warehouse.png" alt="Fig 1">
+        <img src="mode_of_shipment.png" alt="Fig 2">
+        </div>
+        <div style="width:45%;float:left">
+        <img src="average_customer_rating.png" alt="Fig 3">
+        <img src="weight_distribution.png" alt="Fig 4">
+        </div>
+    </body>
+    </html>
     """
-    El archivo `files//shipping-data.csv` contiene información sobre los envios
-    de productos de una empresa. Cree un dashboard estático en HTML que
-    permita visualizar los siguientes campos:
 
-    * `Warehouse_block`
+    # Escribir el contenido en un archivo HTML
+    with open("docs/index.html", "w") as file:
+        file.write(html_content)
 
-    * `Mode_of_Shipment`
+    
 
-    * `Customer_rating`
-
-    * `Weight_in_gms`
-
-    El dashboard generado debe ser similar a este:
-
-    https://github.com/jdvelasq/LAB_matplotlib_dashboard/blob/main/shipping-dashboard-example.png
-
-    Para ello, siga las instrucciones dadas en el siguiente video:
-
-    https://youtu.be/AgbWALiAGVo
-
-    Tenga en cuenta los siguientes cambios respecto al video:
-
-    * El archivo de datos se encuentra en la carpeta `data`.
-
-    * Todos los archivos debe ser creados en la carpeta `docs`.
-
-    * Su código debe crear la carpeta `docs` si no existe.
-
-    """
